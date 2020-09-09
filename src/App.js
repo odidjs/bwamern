01781745b691bdb4d7219b45c5119166dda1bd98
odi-dewangga <odidewangga0697@gmail.com>
2020-09-09 21:13:24 +0700

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import "assets/scss/style.scss";
+
+
+function App() {
+  return (
+    <div className="App">
+      Hai world, I'm front end developper!
+    </div>
+  );
+}
+
+export default App;
